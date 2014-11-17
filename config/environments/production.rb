079e96dd0e91d7c53c67450a2f177f7f9e7d9a2d
Varun Narang <varunarang@gmail.com>
2014-11-17 18:17:54 +0530

    Update the assets precompiler to include common file types

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,11 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile << Proc.new { |path|
+    if path =~ /\.(eot|svg|ttf|woff|png)\z/
+      true
+    end
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
